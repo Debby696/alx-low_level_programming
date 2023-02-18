aa8b14f62f9b4ffafe05e0efea9b929711835a2b
Debby696 <idowuopeyemi696@gmail.com>
2023-02-18 08:07:06 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - prints the alphabet.
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: Get a random it with 5
+ * Return: 0
  */
 int main(void)
 {
-	char alp[26] - "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char c = 'a';
 
-	for (i - 0; i < 26; i++)
+	while (c <= 'z')
 	{
-		putchar(alp[i]);
+		putchar(c);
+		c++;
 	}
+
 	putchar('\n');
 	return (0);
 }
