dcef854d85d86b0ca56e965d68f89d380a88ca76
Debby696 <idowuopeyemi696@gmail.com>
2023-02-18 09:53:13 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,12 +15,14 @@ int main(void)
 	c = 'a';
 	d = 'A';
 	while
-		(c <= 'z') {
+		(c <= 'z')
+	       	{
 			putchar(c);
 			c++;
 		}
 	while
-		(d <= 'z') {
+		(d <= 'z')
+	       	{
 			putchar(d);
 			d++;
 		}
