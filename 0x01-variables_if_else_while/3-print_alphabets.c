d70af8c6a4149c6a79edce22e181454fa88a541e
Debby696 <idowuopeyemi696@gmail.com>
2023-02-18 10:25:31 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,26 +6,17 @@
  *
  * Return: Always 0
  */
-
 int main(void)
 {
-	char c;
-	char d;
+	char ch;
 
-	c = 'a';
-	d = 'A';
-	while
-		(c <= 'z')
-	       	{
-			putchar(c);
-			c++;
-		}
-	while
-		(d <= 'z')
-	       	{
-			putchar(d);
-			d++;
-		}
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar (ch);
+
 	putchar('\n');
+
 	return (0);
 }
