c68ccb18d92d700a4c91d5835fb3d848851e2556
Debby696 <idowuopeyemi696@gmail.com>
2023-02-18 09:12:43 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /**
  * main - prints alpha in lower and upper case
  *
@@ -7,8 +8,7 @@
  */
 int main(void)
 {
-	char ch;
-
+	char (ch);
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 
