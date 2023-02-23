878e6f5f6f8f6601dcd0fe45fefb003e663cedb0
Debby696 <idowuopeyemi696@gmail.com>
2023-02-23 04:10:08 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,8 +1,6 @@
-#include "main.h"
 #include <stdio.h>
-
 /**
- * main - check the code
+ * main - check the code.
  *
  * Return: Always 0.
  */
@@ -10,9 +8,9 @@ int main(void)
 {
     char c;
 
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
     c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
+    printf("%c: %d\n", c, _isupper(c));
     return (0);
 }
