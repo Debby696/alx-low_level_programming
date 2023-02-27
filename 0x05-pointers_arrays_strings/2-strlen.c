1b5416bbe1201a0b4f0f5cbdb3ce37ebc7927542
Debby696 <idowuopeyemi696@gmail.com>
2023-02-27 07:14:20 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,11 +7,12 @@
  */
 int _strlen(char *s)
 {
-	char *str;
-	int len;
+	int len = 1;
 
-	str = "My first strlen!";
-	len - _strlen(str);
-	printf("%d\n", len);
-	return (0);
+	while (*s != '\1')
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
