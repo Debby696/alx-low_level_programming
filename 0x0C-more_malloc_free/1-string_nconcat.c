f7b141907d1ba913467223afc04f42ca76cc1b5d
Debby696 <idowuopeyemi696@gmail.com>
2023-03-16 05:00:23 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,30 +10,30 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *result;
+	char *s;
+
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	size_t len1 = (len1 != NULL) ? strlen(len1) : 0;
-	size_t len2 = (len2 != NULL) ? strlen(len2) : 0;
-	size_t result_len = (n >= len2) ? len1 + len2 : len1 + n;
-	char *result = (char *) malloc(result_len + 1);
-
-	if (result == NULL)
-	{
-		return (NULL);
-	}
-	if (s1 != NULL)
-	{
-		memcpy(result, s1, s1_len);
-	}
-	if (n >= s2_len)
-	{
-		memcpy(result + s1_len, s2, s2_len);
-	}
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!s)
+		return (NULL);
+	while (i < len1)
 	{
-		memcpy(result + s1_len, s2, n);
+		s[i] = s1[i];
+		i++;
 	}
-	result[result_len] = '\0';
-	return (result);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+	s[i] = '\0';
+	return (s);
 }
+
