485a62dfd4d27149a472dda47384da95b061632f
Debby696 <idowuopeyemi696@gmail.com>
2023-04-10 02:10:02 -0700

    update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,8 +7,8 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (indeix > 63)
 		return (-1);
-	*n = *n | (1u << index);
+	*n = *n | ~(1u << index);
 	return (1);
 }
