2c2e01800fc94b252da5326b961a5d0bdd34aee6
Debby696 <idowuopeyemi696@gmail.com>
2023-04-10 08:09:10 -0700

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,30 +1,33 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * create_file - creates file
- * filename: is the name of the file to create
- * @text_content: a pointer to a string to write the file
- * Return: 1 on success, -1 on failure
+ * create_file - Creates  file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: 1 on success -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, w, len = 0;
+	int fd, wr, count = 0;
 
-if (filename == NULL)
-return (-1);
+	if (filename == NULL)
+		return (-1);
 
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-if (fd == -1)
-return (-1);
-if (text_content != NULL)
-{
-	for (len = 0; text_content[len];)
-		len++;
-}
-w = write(fd, text_content, len);
-if (w == -1)
-	return (-1);
+	if (text_content != NULL)
+	{
+		for (count = 0; text_content[count];)
+			count++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(fd, text_content, count);
+
+	if (fd == -1 || wr == -1)
+		return (-1);
+
 	close(fd);
+
 	return (1);
 }
+
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,88 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - copies the content of afile to another
+ * @argc: no of arguments present in the program
+ * @argv: array of pointer to the arguments
+ * Return: 0 if successful
+ * Description: if no of argument is not the correct one, exit with code 97
+ *	if file_from does not exist, or can't be read exit with 98
+ *	if write to file_to fails or can't be created exit with code 99
+ *	unable to close a file descriptor , exit with code 100
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, r, w;
+	char *buf;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	buffer = create_buffer(argv[2]);
+	fd_from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	do {
+		if (fd_from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+		w = write(to, buffer, r);
+		if (fd_to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+		r = read(fd_from, buffer, 1024);
+		fd_to = open(argv[2], O_WRONLY | O_APPEND);
+	} while (r > 0);
+	free(buffer);
+	close_file(fd_from);
+	close_file(fd_to);
+	return (0);
+}
+
+char *create_buffer(char *file);
+void close_file(int fd);
+/**
+ * create buffer - this allocates 1024 bytes for a buffer
+ * @file: file buffer is storing char for
+ * Return: pointer to the new buffer
+ */
+char *create_buffer(char *file)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n" file);
+		exit(99);
+	}
+	return (buffer);
+}
+
+/**
+ * close_file -this closes file descriptors
+ * @fd: the file descriptor to close
+ */
+void close_file(int fd)
+{
+	int cl = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+
+
