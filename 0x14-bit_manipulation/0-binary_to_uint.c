9628a81423d521fe93b1edc24db4aaa515a7439d
Debby696 <idowuopeyemi696@gmail.com>
2023-05-04 15:04:57 -0700

    converts binary to an unsugned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,23 +2,23 @@
 #include <stdio.h>
 
 /**
- * binary_to_unit - converts a binary to an unsigned int
+ * binary_to_uint - converts a binary to an unsigned int
  * @b: pointer to astring of 0 & 1 chars
  * Return: the converted no or 0 if b is null
  * or there is 1 or more chars in the string b thast is not 0 or 1. return 0
  *
  */
-unsigned unt binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int con = 0, i;
-	
+
 	if (!b)
-		retturn (0);
-	for (i = 0; b[i[l i++)
+		return (0);
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		con - 2 *con + (b[i] - '0');
+		con = 2 * con + (b[i] - '0');
 	}
 	return (con);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,14 +1,14 @@
-#ifndef "MAIN.H"
-#define "MAIN.H"
+#ifndef MAIN_H
+#define MAIN_H
 
-unsigned int binary_to_unit(const char *b);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n. unsigned long int m);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianess(void);
-int_atoi(const char *s);
+int _atoi(const char *s);
 int _putchar(char c);
 
 #endif
