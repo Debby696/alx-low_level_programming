dfe6f5f86da499a29e01e903344db357331c7781
Debby696 <idowuopeyemi696@gmail.com>
2023-10-11 14:57:17 -0700

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,7 +5,7 @@
 #include <math.h>
 
 /**
- * struct listint_s - singly linked list
+ * struct listint_s - singily linked list
  *
  * @n: Integer
  * @index: Index of the node in the list
@@ -15,9 +15,9 @@
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 /**
@@ -32,10 +32,10 @@ typedef struct listint_s
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 
