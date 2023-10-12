8cfecf185a35adf2f258008e1a612d9e33a8e9ae
Debby696 <idowuopeyemi696@gmail.com>
2023-10-11 18:00:34 -0700

    searches for a value in a sorted array of integers using the Jump search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,18 +1,29 @@
-#include <math.h>
 #include "search_algos.h"
+#include <math.h>
 
-/* compiling math.h with gcc will require `-lm` */
 size_t min(size_t a, size_t b);
 
 /**
- * jump_search - searches for a value in a sorted array of integers
- * using the Jump search algorithm
+ * min - this returns the minimum of two size_t values
+ * @a: is the first value
+ * @b: is the second value
  *
- * @array: is a pointer to the first element of the array to search in
+ * Return: `a` if lower or equal to `b`, `b` otherwise
+ */
+size_t min(size_t a, size_t b)
+{
+	return (a <= b ? a : b);
+}
+
+/**
+ * jump_search - searches for a value in a sorted array of integers using
+ * a jump search algorithm
+ * @array: is a pointer to first element of array to search
  * @size: is the number of elements in array
- * @value:  is the value to search for
- * Return: 1st index containing `value`, otherwise -1,
- * if value is not in array or array is NULL
+ * @value: is the value to search for
+ *
+ * Return: first index containing `value`, or -1 if `value` not found or
+ * `array` is NULL
  */
 
 int jump_search(int *array, size_t size, int value)
@@ -31,6 +42,7 @@ int jump_search(int *array, size_t size, int value)
 		       high, array[high]);
 	}
 
+	/* causes 'found' msg even when value not in array */
 	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
 
 	for (; low <= min(high, size - 1); low++)
